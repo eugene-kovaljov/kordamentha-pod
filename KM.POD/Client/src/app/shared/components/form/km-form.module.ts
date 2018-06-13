f92ff50b7331da2548d35115ff441e1b5cc2aa35
Eugene Kovaljov <eugene-kovaljov@pluralsight.com>
2018-06-13 10:50:02 +0300

    add initial button for form component

--- KM.POD/Client/src/app/shared/components/form/km-form.module.ts
+++ KM.POD/Client/src/app/shared/components/form/km-form.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from '../../../core/core.module';
 import { FormInputComponent } from './components/form-input/form-input.component';
+import { SharedModule } from '../../shared.module';
 
 @NgModule({
-  imports: [CommonModule, CoreModule],
+  imports: [CommonModule, CoreModule, SharedModule],
   declarations: [FormInputComponent],
   exports: [FormInputComponent]
 })
