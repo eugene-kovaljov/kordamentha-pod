29022899d6ef0b0784a00fe80adc67baa2fc2467
Svetlana Semyonova <s.semyonova@andersenlab.com>
2018-06-13 12:07:19 +0300

    add edit icon, emit button and forms

--- KM.POD/Client/src/app/pages/main/account/account.component.ts
+++ KM.POD/Client/src/app/pages/main/account/account.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { SetPageTitle } from '../main-store/actions';
 import { Store } from '@ngrx/store';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'km-account',
@@ -8,9 +10,24 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
-  constructor(private store: Store<any>) {}
+  @Output() formEmitter = new EventEmitter();
+
+  public accountForms: FormGroup;
+
+  constructor(private store: Store<any>, private fb: FormBuilder) {}
 
   public ngOnInit(): void {
     this.store.dispatch(new SetPageTitle('My Account'));
+    this.accountForms = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phone: [''],
+      password: ['']
+    });
+  }
+
+  public raiseAccountData(): void {
+    this.formEmitter.emit(this.accountForms.getRawValue());
   }
 }
--- KM.POD/Client/src/app/pages/main/account/account.module.ts
+++ KM.POD/Client/src/app/pages/main/account/account.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CoreModule } from '../../../core/core.module';
 import { AccountComponent } from './account.component';
 import { KmFormModule } from '../../../shared/components/form/km-form.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, CoreModule, KmFormModule],
+  imports: [CommonModule, CoreModule, KmFormModule, ReactiveFormsModule, FormsModule],
   declarations: [AccountComponent],
   exports: []
 })
